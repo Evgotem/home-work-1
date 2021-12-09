a11faeddbead9181dad708285887081ec24e42c8
Evgotem <57010853+Evgotem@users.noreply.github.com>
2021-12-09 23:32:23 +0400

    реализовал форму без ресета

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import "bootstrap/dist/css/bootstrap.min.css";
-import App from './tutorial-7/App';
+import App from './tutorial-8/App';
 import { BrowserRouter as Router } from "react-router-dom";
 
 ReactDOM.render(
