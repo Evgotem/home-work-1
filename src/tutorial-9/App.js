f28a3b778f8da687cefbeda02d05d7298ad00e5d
Evgotem <57010853+Evgotem@users.noreply.github.com>
2021-12-22 11:34:06 +0400

    tutorial-9

--- src/tutorial-9/App.js
+++ src/tutorial-9/App.js
@@ -1,31 +1,68 @@
 import React from 'react';
+
 import './style.scss';
+import axios from 'axios';
 
 const App = () => {
   const [users, setUsers] = React.useState([]);
+  const [data, setData] = React.useState({
+    name: '',
+    email: '',
+  });
+
+  const onInputChange = (event) => {
+    const { name, value } = event.target;
+    setData({
+      ...data,
+      [name]: value,
+    });
+  };
 
   const getUsers = async () => {
-    try {
-      const response = await fetch('https://60e0b5b96b689e001788cb8e.mockapi.io/users')
-      const data = await response.json();
-  
-      setUsers(data) 
-    } catch (error) {
-      console.log('ошибка: ', error);
-    } 
-  }
+    const { data } = await axios.get('https://60e0b5b96b689e001788cb8e.mockapi.io/users');
+    setUsers(data);
+  };
+
+  const createUser = () => {
+    if (data.name && data.email) {
+      axios.post('https://60e0b5b96b689e001788cb8e.mockapi.io/users', data);
+    } else {
+      alert ('заполните все поля!')
+    }
+    setData({
+      name: '',
+      email: '',
+    });
+  };
 
   return (
     <div>
       <button onClick={getUsers}>Получить список пользователей</button>
       <button onClick={() => setUsers([])}>Очистить список</button>
       <ul>
-        {users.map(obj => (
-          <li key={obj.id}>{obj.username}</li>
+        {users.map((obj) => (
+          <li key={obj.id}>{obj.name}</li>
         ))}
       </ul>
+      <hr />
+      <h2>Создать пользователя:</h2>
+      <input
+        value={data.name}
+        name="name"
+        type="text"
+        placeholder="Введите имя"
+        onChange={onInputChange}
+      />
+      <input
+        value={data.email}
+        name="email"
+        type="text"
+        placeholder="Введите email"
+        onChange={onInputChange}
+      />
+      <button onClick={createUser}>Добавить пользователя</button>
     </div>
-  )
+  );
 };
 
 export default App;
